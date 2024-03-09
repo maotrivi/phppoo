41aac59362fc75ca2254ed3407728ffaaf6b3049
maotrivi <matproducciones@hotmail.com>
2024-03-08 21:09:11 -0500

    prueba

---
+++ cuenta.php
@@ -0,0 +1,20 @@
+<?php
+
+class Cuenta
+{
+    private $nombre;
+    private $nroCuenta;
+    private $tipoInteres;
+    private $saldo;
+
+    public function_construct($nombre, $nroCuenta, $tipoInteres, $saldo)
+    {
+        $this->nombre = $nombre;
+        $this->nroCuenta = $nroCuenta;
+        $this->$tipoInteres = $tipoInteres;
+        $
+
+
+    }
+
+}
